aac55ff570bbeda6a276c6ff3f81875d75ed5877
patelavtx <apatel@aviatrix.com>
2022-10-16 15:29:28 +0100

    yml added

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@
 provider "aviatrix" {
   controller_ip           = var.controller_ip
   username                = "admin"
-  password                = var.ctrl_password
+  password                = var.ctrl-password
 
 }
 
