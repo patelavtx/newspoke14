c584625b19fc49da260532b04e8d2423ccbb32ff
patelavtx <apatel@aviatrix.com>
2022-10-27 17:38:30 +0100

    new repo

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "atulrg-opstesting"
-    storage_account_name = "atulstorspk17"
-    container_name       = "ctrspk17"
-    key                  = "spk17.tfstate"
+    storage_account_name = "atulstorspk14"
+    container_name       = "ctrspk14"
+    key                  = "spk14.tfstate"
   }
 }
---
+++ readme.txt
@@ -0,0 +1,4 @@
+# Oct 27  >  updated backend and yaml to reflect spoke14 to test with adotest-basic
+
+
+# Remember to revert for pushing to test 'newspoke17'
